d5cb4b3392291a0ffb898fa51d6bb9b6c412b7b1
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-04-24 20:04:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,13 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const routes = require('./routes');
+const logger = require("morgan")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
