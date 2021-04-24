fd6b32fdb19a837624d6929635b5df4e31edfcd6
Gurvinder Dehl <gurvinder360@hotmail.com>
2021-04-24 19:53:44 +0100

    Model completed

--- server.js
+++ server.js
@@ -4,4 +4,34 @@ const WorkoutSchema = new Schema({
     day: {
         type: Date,
         default: Date.now
-    }
+    },
+    exercises: [
+        {
+            type: {
+                type: String,
+            },
+            name: {
+                type: String,
+            },
+            weight: {
+                type: Number,
+            },
+            sets: {
+                type: Number,
+            },
+            reps: {
+                type: Number,
+            },
+            duration: {
+                type: Number,
+            },
+            distance: {
+                type: Number,
+            }
+        }
+    ]
+}, { toJSON: { virtuals: true } });
+
+
+
+module.exports = Workout;
